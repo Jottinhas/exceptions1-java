3ce19cc0ffdc6d039b726f8206dcafecfa386324
Jonatas <jonatascostasouza@gmail.com>
2025-06-06 15:03:37 -0300

    Começo da primeira versão do estudo

---
+++ src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+public class Program {
+    public static void main(String[] args) {
+        //Exceções personalizadas
+        //sugestão de divisão de pacotes 'model'
+        //model - entities - enum - exception - services
+
+
+
+    }
+}
